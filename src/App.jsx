13dce042ae571358bf79acdcece01cb93c12b02c
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-10-07 05:48:50 +0500

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { Data } from './components/data'
+
+
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
